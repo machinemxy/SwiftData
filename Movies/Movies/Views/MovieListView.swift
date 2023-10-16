ad2667fa087ef20afbf61897446fc53c82471a94
xueyuan-ma <xueyuan.ma@rakuten.com>
2023-10-16 13:52:29 +0900

    delete movie

---
+++ Movies/Movies/Views/MovieListView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieListView.swift
+//  Movies
+//
+//  Created by Ma, Xueyuan | Cecil | LIPD on 2023/10/16.
+//
+
+import SwiftUI
+
+struct MovieListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieListView()
+}
