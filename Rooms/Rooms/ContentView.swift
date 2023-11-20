9abdebeda1f9a2b8b1c1497f0335e424a3e1521f
xueyuan-ma <xueyuan.ma@rakuten.com>
2023-11-20 16:12:12 +0900

    Rooms starter

---
+++ Rooms/Rooms/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rooms
+//
+//  Created by Ma, Xueyuan on 2023/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rooms/Rooms/RoomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomsApp.swift
+//  Rooms
+//
+//  Created by Ma, Xueyuan on 2023/11/20.
+//
+
+import SwiftUI
+
+@main
+struct RoomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
