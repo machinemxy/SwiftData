43111427c8b3e6a1bf90dfd0c2a2d2f0eafd5a14
xueyuan-ma <xueyuan.ma@rakuten.com>
2023-12-06 16:53:20 +0900

    Garden Greens - starter

---
+++ GardenGreens/GardenGreens/GardenGreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  GardenGreensApp.swift
+//  GardenGreens
+//
+//  Created by Ma, Xueyuan on 2023/12/06.
+//
+
+import SwiftUI
+
+@main
+struct GardenGreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
