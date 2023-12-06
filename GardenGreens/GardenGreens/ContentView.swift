2692d6ab9679307dc1069be4679e32962c2c842e
xueyuan-ma <xueyuan.ma@rakuten.com>
2023-12-06 17:11:14 +0900

    Garden Greens - basis done

--- GardenGreens/GardenGreens/ContentView.swift
+++ GardenGreens/GardenGreens/ContentView.swift
@@ -6,19 +6,38 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var context
+    @Query private var vegetables: [Vegetable]
+    @State private var name = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Name", text: $name)
+                .textFieldStyle(.roundedBorder)
+                .onSubmit {
+                    guard !name.isEmpty else { return }
+                    let vegetable = Vegetable(name: name)
+                    context.insert(vegetable)
+                    name = ""
+                }
+
+            List(vegetables) { vegetable in
+                Text(vegetable.name)
+            }
+
+            Spacer()
         }
         .padding()
+        .navigationTitle("Garden Greens")
     }
 }
 
 #Preview {
-    ContentView()
+    NavigationStack {
+        ContentView()
+            .modelContainer(for: [Vegetable.self], inMemory: true)
+    }
 }
---
+++ GardenGreens/GardenGreens/Models/Vegetable.swift
@@ -0,0 +1,18 @@
+//
+//  Vegetable.swift
+//  GardenGreens
+//
+//  Created by Ma, Xueyuan on 2023/12/06.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Vegetable {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+}
